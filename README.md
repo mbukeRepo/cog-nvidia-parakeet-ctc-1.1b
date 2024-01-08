836f897bd24035c291273d9579c8fd2737768e0b
mbukeRepo <mbuke404@gmail.com>
2024-01-09 01:36:13 +0200

    chore: initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# cog-nvidia-parakeet-ctc-1.1b
+
+nvidia/parakeet-ctc-1.1b running in Replicate Cog container ⚙️
+
+## Model Description 📝
+
+parakeet-ctc-1.1b is an ASR model that transcribes speech in lower case English alphabet. This model is jointly developed by NVIDIA NeMo and Suno.ai teams.
+
+For more details, visit the model's page on Hugging Face: [nvidia/parakeet-ctc-1.1b](https://huggingface.co/nvidia/parakeet-ctc-1.1b)
---
+++ predict.py
@@ -0,0 +1,25 @@
+# Prediction interface for Cog ⚙️
+# https://github.com/replicate/cog/blob/main/docs/python.md
+
+import os
+import time
+import subprocess
+import nemo.collections.asr as nemo_asr
+from cog import BasePredictor, Input, Path
+
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        self.asr_model = nemo_asr.models.EncDecCTCBPEModel.from_pretrained(model_name="nvidia/parakeet-ctc-1.1b")
+
+    def predict(self, audio_file: Path = Input(description="Input audio file to be transcribed by the ASR model"),) -> str:
+        """Transcribes the input audio file using the ASR model and returns the transcription as a string"""
+
+        # The model returns a tuple of lists, each containing a single string (transcription)
+        transcription = self.asr_model.transcribe([str(audio_file)])
+        if transcription and transcription[0]:
+            return transcription[0][0]
+        else:
+            print("Error: Transcription failed or returned empty result.")
+            return ""
